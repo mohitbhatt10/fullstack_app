0b5979eb47b5788c3848a3d10427d94dbe3741a1
mohitbhatt10 <mohitbhatt10@gmail.com>
2024-01-09 18:02:10 +0530

    Added Cafe-management Application

---
+++ cafe-management/Frontend/src/app/home/home.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { SignupComponent } from '../signup/signup.component';
+import { ForgotPasswordComponent } from '../forgot-password/forgot-password.component';
+import { LoginComponent } from '../login/login.component';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private dialog:MatDialog, private userService: UserService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.userService.checkToken().subscribe((response:any)=>{
+      this.router.navigate(['/cafe/dashboard'])
+    },(error:any)=>{
+      console.log(error);
+    })
+  }
+
+  handleSignupAction(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '600px';
+    this.dialog.open(SignupComponent, dialogConfig);
+    //  this.dialog.open(SignupComponent, {
+    //   width:'500px',
+    //   exitAnimationDuration:'1000ms',
+    //   enterAnimationDuration:'1000ms'
+    // }) 
+  }
+
+  handleForgotPasswordAction(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '600px';
+    this.dialog.open(ForgotPasswordComponent, dialogConfig);
+  }
+
+  handleLoginAction(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '600px';
+    this.dialog.open(LoginComponent, dialogConfig);
+  }
+
+}
